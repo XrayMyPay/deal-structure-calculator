b7717dcbae07d59085c7dff6205bcf2ee80e536f
XrayMyPay <xraymypay@gmail.com>
2025-06-05 10:50:40 -0400

    Add main React app file

---
+++ client/src/App.tsx
@@ -0,0 +1,29 @@
+import { Switch, Route } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import Calculator from "@/pages/calculator";
+
+function Router() {
+  return (
+    <Switch>
+      <Route path="/" component={Calculator} />
+      <Route path="/calculator" component={Calculator} />
+      <Route component={() => <div>Page not found</div>} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Router />
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
